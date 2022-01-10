d2696341e022183f8f50d0bd8362243f02cb447c
Karol Grzybowski <k.grzybowski11@gmail.com>
2022-01-10 11:43:16 +0100

    asd

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,24 @@ public class Main {
         b) w drugiej pętli continue
         c) w trzeciej pętli return
         */
-
+        /* a) */
+        for (int i = 0; i <= 25; i++) {
+            if (i == 13)
+                break;
+            System.out.print(i + " ");
+        }
+        /* b) */
+        for (int i = 0; i <= 10; i++) {
+            if (i % 8 == 0)
+                continue;
+            System.out.print(i + " ");
+        }
+        /* c) */
+        for (int i = 0; i <= 100; i++) {
+            if (i == 33)
+                return;
+            System.out.print(i + " ");
+        }
 
     }
 }
